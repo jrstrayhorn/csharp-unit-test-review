e847fd006b092a151fb359c36f5048a7b4056362
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-01-03 16:27:56 -0500

    finished with writing a simple unit test

---
+++ TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments()
+        {
+            var math = new Math();
+
+            var result = math.Add(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
