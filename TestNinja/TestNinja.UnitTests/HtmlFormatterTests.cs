33f79afded4dcec2d102ff9b4be5ddd3d20991d5
J.R. Strayhorn <jrstrayhorn@gmail.com>
2019-01-04 11:51:34 -0500

    done with testing strings

---
+++ TestNinja/TestNinja.UnitTests/HtmlFormatterTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            // Specific - this good because result is exact string
+            Assert.That(result, Is.EqualTo("<strong>abc</strong>").IgnoreCase);
+
+            // More general - this could test true for bug because too general (need both)
+            // for string its better to be a bit more general unless you need
+            //Assert.That(result, Does.StartWith("<strong>").IgnoreCase);
+            //Assert.That(result, Does.EndWith("</strong>"));
+            //Assert.That(result, Does.Contain("abc"));
+        }
+    }
+}
